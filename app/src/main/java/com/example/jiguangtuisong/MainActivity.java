423d14c7b6105d3e61a4986ec44d8657214bb097
weishuyong1996 <svipwsy@163.com>
2019-05-01 09:35:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jiguangtuisong/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jiguangtuisong;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class MainActivity extends AppCompatActivity {
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+     }
+}
